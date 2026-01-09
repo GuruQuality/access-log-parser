589e21292b573488a8cf31e3c617c128436d9a5a
Katerina <tsvetkova.e.i@mail.ru>
2026-01-09 18:01:59 +0300

    Checked User-Agent

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,7 +13,8 @@ public class Main {
         int count = 1;
         while (true) {
             System.out.println("Введите путь к файлу");
-            String path = new Scanner(System.in).nextLine();
+            //String path = new Scanner(System.in).nextLine();
+            String path = "/Users/kate/IdeaProjects_JAVA/access-log-parser/src/test/access.log";
             File file = new File(path);
             boolean objectExists = file.exists();//Проверяем сущетсвет ли путь
             if (!objectExists) {
@@ -35,26 +36,83 @@ public class Main {
                 BufferedReader reader =
                         new BufferedReader(fileReader);
                 String line;
-                Integer maxLength = 0;
-                Integer minLength = 1024;
+//                Integer maxLength = 0;
+//                Integer minLength = 1024;
                 Integer allLine = 0;
+                String yandexBot = "YandexBot";
+                int quantityYandexBot = 0;
+                String googleBot = "Googlebot";
+                int quantityGooglebot = 0;
                 while ((line = reader.readLine()) != null) {
                     int length = line.length();// Подсчет символов в строке
-                    //System.out.println(length);
-                    maxLength = Math.max(maxLength, length);
-                    minLength = Math.min(minLength, length);
+
+                    // 1. Ищем содержимое в первых скобках
+                    int startIndex = line.lastIndexOf('(');
+                    int endIndex = line.lastIndexOf(')');
+                    if (startIndex == -1 || endIndex == -1) {
+                        continue;
+                    }
+                    if (startIndex > endIndex) {
+                        System.out.println(line);
+                        break;
+                    }
+//                    //System.out.println(length);
+//                    maxLength = Math.max(maxLength, length);
+//                    minLength = Math.min(minLength, length);
+
+                    // 1.1 Извлекаем содержимое первых скобок
+                    String firstBrackets = line.substring(startIndex, endIndex);
+
+                    // 2. Разделение по точке с запятой
+                    String[] parts = firstBrackets.split(";");
+
+                    // 3. Очищаем все фрагменты от пробелов
+                    for (int i = 0; i < parts.length; i++) {
+                        parts[i] = parts[i].trim(); // Удаляем пробелы в начале и конце
+                    }
+
+                    //4. Берем второй фрагмент;
+                    if (parts.length >= 2) {
+                        String fragment = parts[1];
+
+                        //5. Отделим в этом фрагменте часть до слэша
+                        int slashIndex = fragment.indexOf('/');
+                        String nameBot = fragment;
+                        if (slashIndex != -1) {
+                            nameBot = fragment.substring(0, slashIndex);//фрагмент до слеша
+                        }
+
+                        //Проверка фрагмента
+                        if (nameBot.equals(yandexBot)) {
+                            quantityYandexBot++;
+                        }
+
+                        if (nameBot.equals(googleBot)) {
+                            quantityGooglebot++;
+                        }
+                        //System.out.println(nameBot);
+                    }
+
                     allLine++;
                     if (length > 1024) {
                         throw new LimitLineLengthException("В строке более 1024 символов");
                     }
                 }
-                System.out.println("Длина самой длинной строки в файле: " + maxLength);
-                System.out.println("Длина самой короткой строки в файле: " + minLength);
+//                System.out.println("Длина самой длинной строки в файле: " + maxLength);
+//                System.out.println("Длина самой короткой строки в файле: " + minLength);
                 System.out.println("Общее количество строк в файле: " + allLine);
+                System.out.println();
+                System.out.println("Количество запросов от YandexBot: " + quantityYandexBot);
+                System.out.println("Количество запросов от GoogleBot: " + quantityGooglebot);
+                System.out.println();
+                System.out.println("Доля запросов от YandexBot: " + (float) quantityYandexBot / allLine * 100);
+                System.out.println("Доля запросов от GoogleBot: " + (float) quantityGooglebot / allLine * 100);
+
+                break;
             } catch (IOException e) {// Ловим исключения ввода и вывода
                 throw new RuntimeException(e);
             } catch (LimitLineLengthException e) {// Ловим исключения по длинне строки
-               throw new RuntimeException(e);
+                throw new RuntimeException(e);
             }
         }
     }
